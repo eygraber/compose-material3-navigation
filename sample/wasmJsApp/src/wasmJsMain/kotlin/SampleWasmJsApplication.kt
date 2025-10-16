7abc231663fbf13bff5963f6e1f822bcd7db3a69
Eliezer Graber <me@eygraber.com>
2025-10-15 22:52:22 -0400

    Use ComposeViewport fo wasm-js sample

--- sample/wasmJsApp/src/wasmJsMain/kotlin/SampleWasmJsApplication.kt
+++ sample/wasmJsApp/src/wasmJsMain/kotlin/SampleWasmJsApplication.kt
@@ -1,8 +1,8 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.window.CanvasBasedWindow
+import androidx.compose.ui.window.ComposeViewport
 import com.eygraber.compose.material3.navigation.sample.shared.App
 
 @OptIn(ExperimentalComposeUiApi::class)
-fun main() = CanvasBasedWindow("Bottom Sheet Navigation Sample") {
+fun main() = ComposeViewport {
   App()
 }
